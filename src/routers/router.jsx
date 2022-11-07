0783e2d7ff422f13b7e9fdd373458ed066c88255
shaquille19 <shakil.m.19@gmail.com>
2022-11-07 22:05:43 +0600

    set up basic layout and routes

---
+++ src/routers/router.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/Home/Home";
+import Main from "../layout/Main";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+export default router;
