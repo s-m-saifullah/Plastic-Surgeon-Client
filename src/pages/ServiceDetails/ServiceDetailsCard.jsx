43e34abce5810316fc8a50cb9f12ee87f181670a
shaquille19 <shakil.m.19@gmail.com>
2022-11-08 11:10:31 +0600

    added ratign display

--- src/pages/ServiceDetails/ServiceDetailsCard.jsx
+++ src/pages/ServiceDetails/ServiceDetailsCard.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { FaStar, FaRegStar } from "react-icons/fa";
 
 const ServiceDetailsCard = ({ service }) => {
   const { _id, name, photoUrl, price, rating, description } = service;
+
   return (
     <div className="flex justify-center">
       <div className="flex flex-col md:flex-row rounded-lg bg-white shadow-lg">
@@ -11,12 +13,22 @@ const ServiceDetailsCard = ({ service }) => {
           alt=""
         />
         <div className="p-6 flex flex-col justify-start">
-          <h5 className="text-gray-900 text-3xl font-medium mb-2">{name}</h5>
+          <h5 className="text-gray-900 text-6xl font-medium mb-2">{name}</h5>
           <p className="text-gray-700 text-lg mb-4">{description}</p>
           <p className="text-primary mb-10">
             <span className="text-7xl">${price}</span>
           </p>
-          <p>Ratings: </p>
+          <p className="flex items-center text-lg">
+            <span className="mr-3">Ratings:</span>
+            <span className="flex text-primary">
+              {[...Array(parseInt(rating))].map((item, index) => (
+                <FaStar key={index} />
+              ))}
+              {[...Array(5 - parseInt(rating))].map((item, index) => (
+                <FaRegStar key={index} />
+              ))}
+            </span>
+          </p>
         </div>
       </div>
     </div>
