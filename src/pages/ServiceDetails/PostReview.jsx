e085754074e7b7d522c268a0eec4a7fb26a34dc6
shaquille19 <shakil.m.19@gmail.com>
2022-11-10 19:55:14 +0600

    changed theme

--- src/pages/ServiceDetails/PostReview.jsx
+++ src/pages/ServiceDetails/PostReview.jsx
@@ -66,7 +66,7 @@ const PostReview = ({ id, serviceName, count, setCount }) => {
         <div className="flex items-stretch mb-6"></div>
         <button
           type="submit"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-base w-full px-5 py-2.5 text-center"
+          className="text-white bg-primary hover:bg-lime-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-base w-full px-5 py-2.5 text-center"
         >
           Add Review
         </button>
