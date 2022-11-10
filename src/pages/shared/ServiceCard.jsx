e850605adcc859f9900e321cc93600c9efa271a1
shaquille19 <shakil.m.19@gmail.com>
2022-11-10 09:15:44 +0600

    implemented photo lightbox

--- src/pages/shared/ServiceCard.jsx
+++ src/pages/shared/ServiceCard.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import { PhotoProvider, PhotoView } from "react-photo-view";
 import { Link } from "react-router-dom";
+import "react-photo-view/dist/react-photo-view.css";
 
 const ServiceCard = ({ service }) => {
   const { _id, name, photoUrl, price, description } = service;
   return (
     <div className="card w-full bg-base-100 shadow-xl">
       <figure>
-        <img src={photoUrl} alt={name} />
+        <PhotoProvider>
+          <PhotoView src={photoUrl}>
+            <img src={photoUrl} alt={name} />
+          </PhotoView>
+        </PhotoProvider>
       </figure>
       <div className="card-body gap-4">
         <h2 className="card-title">{name}</h2>
